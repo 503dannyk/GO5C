aaad9c3998637ad7575b74ce3034cdd58d900b73
503dannyk <danielcastillo@Daniels-MacBook-Pro.local>
2015-11-13 21:32:30 -0800

    New working version

---
+++ app/src/main/java/dtindustries/go5c/login.java
@@ -0,0 +1,94 @@
+package dtindustries.go5c;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class login extends Activity implements View.OnClickListener {
+    private EditText user, pass;
+    private Button bLogin;
+    // Progress Dialog
+    private ProgressDialog pDialog;
+    //JSON parser class
+    JSONParser jsonParser = new JSONParser();
+    private static final String LOGIN_URL = "http://10.0.2.2/5CGO/login.php";
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        user = (EditText) findViewById(R.id.edtuserid);
+        pass = (EditText) findViewById(R.id.edtpass);
+        bLogin = (Button) findViewById(R.id.btLogin);
+        bLogin.setOnClickListener(this);
+    }
+
+    @Override public void onClick(View v) {
+        // TODO Auto-generated method
+        switch (v.getId()) {
+            case R.id.btLogin:
+                String username = user.getText().toString();
+                String password = pass.getText().toString();
+                new AttemptLogin().execute(username, password); // here we have used, switch case, because on login activity you may //also want to show registration button, so if the user is new ! we can go the //registration activity , other than this we could also do this without switch //case.
+            default: break;
+        }
+    }
+
+    class AttemptLogin extends AsyncTask< String, String, String > { /** * Before starting background thread Show Progress Dialog * */
+
+    boolean failure = false;@Override protected void onPreExecute() {
+        super.onPreExecute();
+        pDialog = new ProgressDialog(login.this);
+        pDialog.setMessage("Attempting for login...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(true);
+        pDialog.show();
+    }
+        @Override protected String doInBackground(String...args) { // TODO Auto-generated method stub // here Check for success tag
+            int success;
+            try {
+                List< NameValuePair > params = new ArrayList< NameValuePair >();
+                params.add(new BasicNameValuePair("username", args[0]));
+                params.add(new BasicNameValuePair("password", args[1]));
+                Log.d("request!", "starting");
+                JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST", params); // checking log for json response
+                Log.d("Login attempt", json.toString());
+                // success tag for
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Successfully Login!", json.toString());
+                    Intent ii = new Intent(login.this, main_activity.class);
+                    finish(); // this finish() method is used to tell android os that we are done with current //activity now! Moving to other activity startActivity(ii);
+                    return json.getString(TAG_MESSAGE);
+                } else {
+                    return json.getString(TAG_MESSAGE);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        } /** * Once the background process is done we need to Dismiss the progress dialog asap * **/
+        protected void onPostExecute(String message) {
+            pDialog.dismiss();
+            if (message != null) {
+                Toast.makeText(login.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/dtindustries/go5c/main_activity.java
@@ -0,0 +1,13 @@
+package dtindustries.go5c;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class main_activity extends ActionBarActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main_activity);
+        }
+    }
