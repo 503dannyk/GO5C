32c17b53aaf2afd95f8970ee007f5e9234c2260d
tonyramos <rar32013@pomona.edu>
2016-01-02 10:45:11 -0800

    last commit

---
+++ app/src/main/java/dtindustries/go5c/profileDisplay.java
@@ -0,0 +1,14 @@
+package dtindustries.go5c;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class profileDisplay extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_display);
+    }
+
+}
---
+++ app/src/main/java/dtindustries/go5c/usersAttendingEvent.java
@@ -0,0 +1,14 @@
+package dtindustries.go5c;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class usersAttendingEvent extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_attending_event);
+    }
+
+}
